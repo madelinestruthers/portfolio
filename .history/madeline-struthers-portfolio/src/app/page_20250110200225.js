528a89d8dce228ea3a55bf970191ef09e41e9765
madelinestruthers <mstruthers1@my.bcit.ca>
2025-01-10 21:53:55 -0800

    bruh

---
+++ .history/madeline-struthers-portfolio/src/app/page_20250110200225.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col items-center gap-4">
+          {/* Face with Animated Hard Hat */}
+          <div className="relative w-32 h-32">
+            {/* Hard Hat */}
+            <div className="absolute top-0 left-4 w-24 h-12 bg-yellow-400 rounded-t-full animate-bounce z-10">
+             
+            </div>
+            {/* Face */}
+            <div className="absolute bottom-0 left-4 w-24 h-24 bg-pink-100 rounded-full flex items-center justify-center z-0">
+              <div className="w-2 h-2 bg-black rounded-full mr-2"></div>
+              <div className="w-2 h-2 bg-black rounded-full"></div>
+            </div>
+          </div>
+          {/* Text */}
+          <div className="text-2xl font-bold text-white">
+            Under Construction!
+          </div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <div>foot</div>
+      </footer>
+    </div>
+  );
+}
